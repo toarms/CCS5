0605ae186c9586de2a4befa27eed1f000b6c3fa1
toarms <kkmm99@gmail.com>
2014-05-11 17:31:01 +0800

    F247 seems reboot when uart transfer; add analysis.py for F247 uart test

--- F247_OF_XT2/OX_XT2.c
+++ F247_OF_XT2/OX_XT2.c
@@ -1,84 +1,19 @@
-/* --COPYRIGHT--,BSD_EX
- * Copyright (c) 2012, Texas Instruments Incorporated
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *******************************************************************************
- *
- *                       MSP430 CODE EXAMPLE DISCLAIMER
- *
- * MSP430 code examples are self-contained low-level programs that typically
- * demonstrate a single peripheral function or device feature in a highly
- * concise manner. For this the code may rely on the device's power-on default
- * register values and settings such as the clock configuration and care must
- * be taken when combining code from several examples to avoid potential side
- * effects. Also see www.ti.com/grace for a GUI- and www.ti.com/msp430ware
- * for an API functional library-approach to peripheral configuration.
- *
- * --/COPYRIGHT--*/
-//******************************************************************************
-//   MSP430x249 Demo - XT2 Oscillator Fault Detection
-//
-//   Description: System runs normally in LPM3 with with basic timer clocked by
-//   ACLK.  TimerA0 interrupt causes an exit from LPM3, and toggles P1.0.
-//   If an XT2 oscillator fault occurs, NMI is requested, pausing execution and
-//   flashing LED quickly until fault is resolved.  Assumed only XT2 as NMI
-//   source - code does not check for other NMI sources. MCLK is buffered on
-//   P5.4. If fault occurs for crystal osc sourcing the MCLK, MCLK is
-//   automatically switched to DCO for its clock source
-//   MCLK = XT2 = 8MHz normally, within ISRs MCLK = DCO freq ~1048kHz
-//   //* An external 8MHx crystal is required between XT2 and XT2OUT , and
-//   an external 32kHz crystal is required between XIN and XOUT.  *//
-//
-//                MSP430x249
-//             -----------------
-//         /|\|              XIN|-
-//          | |                 | 32KHz
-//          --|RST          XOUT|-
-//            |                 |
-//            |            XT2IN|-
-//            |                 | 8MHz
-//            |           XT2OUT|-
-//            |                 |
-//            |             P1.0|-->LED
-//            |        P5.4/MCLK|-->MCLK = XT2 HFXTAL
-//
-//  B. Nisarga
-//  Texas Instruments Inc.
-//  September 2007
-//  Built with CCE Version: 3.2.0 and IAR Embedded Workbench Version: 3.42A
-//******************************************************************************
+
 #include <msp430.h>
 
 #define PB_PIN		BIT1	// push button:		P1.1
 #define IR_RX_PIN	BIT2	// ir red led rx: 	p1.2
 #define RED_RX_PIN	BIT3	// red led rx: 		p1.3
+
+#define RED_LED_FLAG	0
+#define IR_LED_FLAG		1
+
+// the result
+unsigned int start_counter;
+unsigned int last_counter;
+unsigned int pulse_counts;
+unsigned int rcounter, icounter;
+unsigned int rcounts=999, icounts=777;
 
 void UART_TX(char * tx_data);
 
@@ -87,13 +22,6 @@ int main(void)
   volatile unsigned int i;
 
   WDTCTL = WDTPW+WDTHOLD;                   // Stop WDT
-
-  /*
-  DCOCTL = 0;                               // Select lowest DCOx and MODx settings
-  BCSCTL1 = CALBC1_1MHZ;                    // Set DCO
-  DCOCTL = CALDCO_1MHZ;
-*/
-
 
   BCSCTL1 &= ~XT2OFF;                       // Activate XT2 high freq xtal
   BCSCTL3 |= XT2S_2;                        // 3 ?16MHz crystal or resonator
@@ -117,7 +45,7 @@ int main(void)
   // Timer A
   //Initialize Timer A to generate one-second interrupt
  // TACCTL0 = CCIE;                           // TACCR0 interrupt enabled
-  //TACCR0 = 32767;                           // Set CCR0 to 32767(ACLK=32.768Hz)
+  //TACCR0 = 32767;                            // Set CCR0 to 32767(ACLK=32.768Hz)
  // TACTL = TASSEL_1 + MC_1;                  // ACLK, up mode
 
 
@@ -137,6 +65,8 @@ int main(void)
 
   // Init ports
   P3DIR |= BIT4;                            // Set led to output direction
+  P4SEL |= BIT1;				//P4.1 TimerB capture
+
   // RED an IR RED RX PINs
   P1DIR &= ~(RED_RX_PIN + IR_RX_PIN);
   P1REN |= (RED_RX_PIN + IR_RX_PIN);
@@ -155,10 +85,53 @@ int main(void)
   }
 }
 
+
+void get_sensor_data(unsigned int led)
+{
+	unsigned int counter = 0x0009;
+	unsigned int temp;
+
+    TBCTL = TBSSEL_2 + MC_2 + TBCLR;
+    TBCCTL1 = CM_1 + SCS + CAP;	// set for caputre on rising edge
+
+    // wait until capture event occurs
+    while(!(TBCCTL1 & CCIFG));
+    TBCCTL1 &= ~CCIFG;			// clear the interrupt flag
+    temp = TBCCR1;				// take the CCR value
+    // set P4.1 to capture the next rising edge
+    TBCCTL1 = CM_1 + SCS + CAP;
+    //wait until capture event occurs
+    while(!(TBCCTL1 & CCIFG));
+    // switch off timer capture and caculate the frequency
+    counter = TBCCR1 - temp;
+    // stop timer
+    TBCCTL1 &= ~CCIFG;
+    TBCTL = 0;
+
+    if(led == RED_LED_FLAG){
+    	rcounter = counter;
+    	rcounts++;
+    }
+    else {
+    	icounter = counter;
+    	icounts++;
+    }
+  }
+
+
 void UART_TX(char * tx_data) // Define a function which accepts a character pointer to an array
 {
-	unsigned int temp;
-	unsigned int counter = 0x0009;
+
+	unsigned int counter, counts;
+
+	if(tx_data[0] == 'i')
+	{
+		counter = icounter;
+		counts = icounts;
+	} else {
+		counter = rcounter;
+		counts = rcounts;
+	}
 
     //unsigned int i=0;
 	while (!(UC1IFG&UCA1TXIFG));
@@ -166,56 +139,21 @@ void UART_TX(char * tx_data) // Define a function which accepts a character poin
     while (!(UC1IFG&UCA1TXIFG));
     UCA1TXBUF = 0xFF;				// Byte 2 - 0xFF (synchronization byte)
 
-    while (!(UC1IFG&UCA1TXIFG));
+    while (!(UC1IFG&UCA1TXIFG));	// "ro" / "io"
     UCA1TXBUF = tx_data[0];
     while (!(UC1IFG&UCA1TXIFG));
     UCA1TXBUF = tx_data[1];
 
-
-
-    if(tx_data[1] == 'o')
-    {
-
-    TBCTL = TBSSEL_2 + MC_2 + TBCLR;
-
-	P4SEL |= BIT1;				//P4.1 TimerB capture
-
-	TBCCTL1 = CM_1 + SCS + CAP;	// set for caputre on rising edge
-
-
-// wait until capture event occurs
-	while(!(TBCCTL1 & CCIFG));
-	TBCCTL1 &= ~CCIFG;			// clear the interrupt flag
-	temp = TBCCR1;				// take the CCR value
-
-	// set P4.1 to capture the next rising edge
-	TBCCTL1 = CM_1 + SCS + CAP;
-
-	//wait until capture event occurs
-	while(!(TBCCTL1 & CCIFG));
-
-	// switch off timer capture and caculate the frequency
-	counter = TBCCR1 - temp;
-
-	TBCCTL1 &= ~CCIFG;
-	TBCTL = 0;
-    }
-
-
-    while (!(UC1IFG&UCA1TXIFG));
+    while (!(UC1IFG&UCA1TXIFG));	// counter
     UCA1TXBUF = counter & 0x00FF;
     while (!(UC1IFG&UCA1TXIFG));
-    UCA1TXBUF = 0x12;
+    UCA1TXBUF = (counter >> 8) & 0x00FF;
+
+    while (!(UC1IFG&UCA1TXIFG));	// counts
+    UCA1TXBUF = counts & 0x00FF;
     while (!(UC1IFG&UCA1TXIFG));
-    UCA1TXBUF = 0x13;
-/*
-    while(tx_data[i]) // Increment through array, look for null pointer (0) at end of string
-    {
-    	while (!(UC1IFG&UCA1TXIFG)); // Wait if line TX/RX module is busy with data
-        UCA1TXBUF = tx_data[i]; // Send out element i of tx_data array on UART bus
-        i++; // Increment variable for array address
-    }
-*/
+    UCA1TXBUF = (counts >> 8) & 0x00FF;
+
 }
 
 #pragma vector=NMI_VECTOR
@@ -240,6 +178,7 @@ __interrupt void Timer_A (void)
   _BIC_SR_IRQ(LPM3_bits+GIE);               // Exit LPM3 upon ISR exit
 }
 
+
 /**************************************************************************//**
 *
 * Port1_Isr
@@ -259,7 +198,7 @@ __interrupt void Port1_Isr(void)
   {
     // change output pin
 	if(P1IN & RED_RX_PIN)
-		UART_TX("ro");
+		get_sensor_data(RED_LED_FLAG);
 	else
 		UART_TX("rf");
 
@@ -272,7 +211,7 @@ __interrupt void Port1_Isr(void)
   {
 	// change output pin
 	if(P1IN & IR_RX_PIN)
-	    UART_TX("io");
+		get_sensor_data(IR_LED_FLAG);
     else
 	    UART_TX("if");
 
---
+++ analysis.py
@@ -0,0 +1,75 @@
+#! python3
+import serial
+import struct
+from datetime import datetime
+
+ser = serial.Serial(4, 115200, timeout=1)
+
+f = open('C:/Users/PC/Desktop/hello.log', 'w')
+f2 = open('C:/Users/PC/Desktop/hello2.log', 'w')
+
+k=0
+j=0
+
+last = datetime.utcnow()
+for i in range(1,10000000):
+ b = ser.read()
+ print("(1):" , b);
+ if b == b'\x00': 
+#   print("(2):", b);
+   b2 = ser.read()
+   print("(3):", b2);
+   if b2 == b'\xff':
+#     print("(4):", b2);
+     this = datetime.utcnow()
+     c = this - last
+     last = datetime.utcnow()
+     value = (this, c.microseconds)
+     s = str(value)
+     f.write(s)
+     f.write("\t\t\t\n")
+     f2.write(s)
+     f2.write("\t\t\t\n")
+
+     b = ser.read(2)
+     value = (b)
+     s2 = str(value)
+
+     freq_LSB = ser.read()
+     freq_HSB = ser.read()
+     count_LSB = ser.read()
+     count_HSB = ser.read()
+     if b == b'if':
+       k += 1
+       freq = ord(freq_HSB) * 256 + ord(freq_LSB)
+       f.write("if: ");
+       f.write(str(k))
+       f.write(" ")
+       f.write(str(freq))
+       f.write(" ")
+       f.write(str(ord(count_HSB)))
+       f.write(" ")
+       f.write(str(ord(count_LSB)))
+       f.write("\n")
+
+     if b == b'rf':
+       j += 1
+       freq = ord(freq_HSB) * 256 + ord(freq_LSB)
+       f2.write("rf: ");
+       f2.write(str(j))
+       f2.write(" ")
+       f2.write(str(freq))
+       f2.write(" ")
+       f2.write(str(ord(count_HSB)))
+       f2.write(" ")
+       f2.write(str(ord(count_LSB)))
+       f2.write("\n")
+
+#     f.write(s)
+
+
+
+# ss = str(value)
+# f.write(ss)
+# f.write('\n')
+
